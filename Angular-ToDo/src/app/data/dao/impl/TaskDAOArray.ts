368cf3b058fba11e043068291ff5871a944e4d27
Artem <ignatev.artem.nik@gmail.com>
2021-06-21 13:30:56 +0400

    Реализован список задач и категорий с помощью DAO

--- Angular-ToDo/src/app/data/dao/impl/TaskDAOArray.ts
+++ Angular-ToDo/src/app/data/dao/impl/TaskDAOArray.ts
@@ -1,48 +1,52 @@
 import {TaskDAO} from "../interface/TaskDAO";
 import {Category} from "../../../model/Category";
-import {Observable} from "rxjs";
+import {Observable, of} from "rxjs";
 import {Priority} from "../../../model/Priority";
 import {Task} from 'src/app/model/Task';
+import {TestData} from "../../TestData";
 
 export class TaskDAOArray implements TaskDAO {
-    add(T): Observable<Task> {
-        return undefined;
-    }
 
-    delete(id: number): Observable<Task> {
-        return undefined;
-    }
 
-    get(id: number): Observable<Task> {
-        return undefined;
-    }
+  getAll(): Observable<Task[]> {
+    return of(TestData.tasks);
+  }
 
-    getAll(): Observable<Task[]> {
-        return undefined;
-    }
+  get(id: number): Observable<Task> {
+    return undefined;
+  }
 
-    getCompletedCountInCategory(category: Category): Observable<number> {
-        return undefined;
-    }
 
-    getTotalCount(): Observable<number> {
-        return undefined;
-    }
+  add(T): Observable<Task> {
+    return undefined;
+  }
 
-    getTotalCountInCategory(category: Category): Observable<number> {
-        return undefined;
-    }
+  delete(id: number): Observable<Task> {
+    return undefined;
+  }
 
-    getUncompletedCountInCategory(category: Category): Observable<number> {
-        return undefined;
-    }
+  getCompletedCountInCategory(category: Category): Observable<number> {
+    return undefined;
+  }
 
-    search(category: Category, searchText: string, status: boolean, priority: Priority): Observable<Task[]> {
-        return undefined;
-    }
+  getTotalCount(): Observable<number> {
+    return undefined;
+  }
 
-    update(T): Observable<Task> {
-        return undefined;
-    }
+  getTotalCountInCategory(category: Category): Observable<number> {
+    return undefined;
+  }
+
+  getUncompletedCountInCategory(category: Category): Observable<number> {
+    return undefined;
+  }
+
+  search(category: Category, searchText: string, status: boolean, priority: Priority): Observable<Task[]> {
+    return undefined;
+  }
+
+  update(T): Observable<Task> {
+    return undefined;
+  }
 
 }
