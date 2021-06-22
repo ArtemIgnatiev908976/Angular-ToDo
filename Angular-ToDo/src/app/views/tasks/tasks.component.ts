383faac75dcebbdac8d10336f42f71774d0a4892
Artem <ignatev.artem.nik@gmail.com>
2021-06-23 00:15:43 +0400

    Действия над задачей
    Добавление иконок редактирования удаления и выполнения с функционалом

--- Angular-ToDo/src/app/views/tasks/tasks.component.ts
+++ Angular-ToDo/src/app/views/tasks/tasks.component.ts
@@ -6,6 +6,8 @@ import {MatPaginator} from "@angular/material/paginator";
 import {MatSort} from "@angular/material/sort";
 import {EditTaskDialogComponent} from "../../dialog/edit-task-dialog/edit-task-dialog.component";
 import {MatDialog} from "@angular/material/dialog";
+import {ConfirmDialogComponent} from "../../dialog/confirm-dialog/confirm-dialog.component";
+
 
 @Component({
   selector: 'app-tasks',
@@ -16,7 +18,7 @@ export class TasksComponent implements OnInit {
 
 
   // поля для таблицы (те, что отображают данные из задачи - должны совпадать с названиями переменных класса)
-  public displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
+  public displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category', 'operations', 'select'];
   public dataSource: MatTableDataSource<Task>; // контейнер - источник данных для таблицы
 
   // ссылки на компоненты таблицы
@@ -155,6 +157,26 @@ export class TasksComponent implements OnInit {
       }
 
     });
+  }
+
+  // диалоговое окно подтверждения удаления
+  public openDeleteDialog(task: Task) {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      maxWidth: '500px',
+      data: {dialogTitle: 'Подтвердите действие', message: `Вы действительно хотите удалить задачу: "${task.title}"?`},
+      autoFocus: false
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) { // если нажали ОК
+        this.deleteTask.emit(task);
+      }
+    });
+  }
+
+  public onToggleStatus(task: Task) {
+    task.completed = !task.completed;
+    this.updateTask.emit(task);
   }
 
 
